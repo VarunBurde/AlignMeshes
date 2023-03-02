09a616b9b45755d2f67cb3142866feeca98b62cd
abenbihi <assia.benbihi@gmail.com>
2023-03-02 22:16:42 +0100

    colmap icp params ok (up to symmetries)

--- utils/align_scale_colmap.py
+++ utils/align_scale_colmap.py
@@ -3,8 +3,10 @@ import copy
 import numpy as np
 import os
 import pymeshlab
+import json
 
-root_path = os.getcwd()
+#root_path = os.getcwd()
+root_path = "/home/abenbihi/ws/tf/sdfstudio/outputs/shared_data/Clean_meshes/"
 mesh_path = os.path.join(os.path.split(root_path)[0], 'meshes')
 gt_meshes = os.path.join(os.path.join(mesh_path, 'gt_mesh'))
 reconstructed_mesh = os.path.join(mesh_path, 'reconstructed_mesh')
@@ -13,6 +15,7 @@ original_mesh_path = os.path.join(colmap_mesh_path, 'original')
 scaled_mesh_path = os.path.join(colmap_mesh_path, 'scaled_mesh')
 aligned_mesh_path = os.path.join(colmap_mesh_path, 'aligned_mesh')
 
+DEBUG = (0==1)
 
 def draw_registration_result(source, target, transformation):
     source_temp = copy.deepcopy(source)
@@ -22,61 +25,152 @@ def draw_registration_result(source, target, transformation):
     source_temp.transform(transformation)
     o3d.visualization.draw_geometries([source_temp, target_temp])
 
+def deg2rad(a_deg):
+    return np.pi * a_deg / 180.0
 
-gt_file_list = []
-for file_name in os.listdir(gt_meshes):
-    if file_name[-4:] == '.ply':
-        gt_file_list.append(file_name)
+def main():
+    with open("./params/colmap_icp.json", "r") as f:
+        icp_params = json.load(f)
+    
+    #print("original_mesh_path: ", original_mesh_path)
+    print("\ngt_meshes: ", gt_meshes)
+    gt_file_list = []
+    for file_name in sorted(os.listdir(gt_meshes)):
+        if file_name[-4:] == '.ply':
+            gt_file_list.append(file_name)
 
-# scaling 'to real world scale
-for file in os.listdir(original_mesh_path):
-    print("processing file : ", file)
-    file_obj = file.replace('.ply', '.obj')
-    scale = int(300 * 0.77)
-    file_path = os.path.join(original_mesh_path, file)
-    mesh = o3d.io.read_triangle_mesh(file_path)
-    box = o3d.geometry.OrientedBoundingBox()
-    crop_box = box.get_axis_aligned_bounding_box()
-    crop_box.min_bound = [-300, -300, -300.0]
-    crop_box.max_bound = [300, 300, -4.5]
-    bb_pcd = mesh.crop(crop_box)
-    bb_pcd.compute_vertex_normals()
-    o3d.io.write_triangle_mesh(mesh=bb_pcd, filename=os.path.join(scaled_mesh_path, file_obj), write_triangle_uvs=True)
+    #print("gt_file_list")
+    #print(gt_file_list)
+    
+    # scaling 'to real world scale
+    rec_meshes = sorted(os.listdir(original_mesh_path))
+    print("\nrec_meshes")
+    print(rec_meshes)
+    
+    if (1==1):
+        for file in rec_meshes:
+            print("processing file : ", file)
+            file_obj = file.replace('.ply', '.obj')
+            #scale = int(300 * 0.77) # TODO: not used?
 
-# Aligning to ground truth frame
-for file in os.listdir(scaled_mesh_path):
-    print("processing file : ", file)
-    file_path = os.path.join(scaled_mesh_path, file)
-    mesh = o3d.io.read_triangle_mesh(file_path)
-    aligned_mesh = os.path.join(aligned_mesh_path, file)
-    # find gt mesh
-    for gt_name in gt_file_list:
-        if gt_name[-6:-4] == file[0:2]:
-            gt_file_path = os.path.join(gt_meshes, gt_name)
-            gt_mesh = o3d.io.read_triangle_mesh(gt_file_path)
+            # path to raw reconstructed mesh
+            file_path = os.path.join(original_mesh_path, file)
 
-    pcd_mesh = mesh.sample_points_uniformly(number_of_points=5000)
-    pcd_gt_mesh = gt_mesh.sample_points_uniformly(number_of_points=5000)
+            # load raw reconstructured mesh
+            mesh = o3d.io.read_triangle_mesh(file_path)
+            box = o3d.geometry.OrientedBoundingBox()
+            crop_box = box.get_axis_aligned_bounding_box()
+            crop_box.min_bound = [-300, -300, -300.0]
+            crop_box.max_bound = [300, 300, -4.5]
 
-    trans_init = np.eye(4)
-    draw_registration_result(pcd_mesh, pcd_gt_mesh, trans_init)
+            # crop the reconstructed mesh
+            bb_pcd = mesh.crop(crop_box)
+            bb_pcd.compute_vertex_normals()
 
-    threshold = 10
-    criteria = o3d.pipelines.registration.ICPConvergenceCriteria()
-    criteria.max_iteration = 3000
-    criteria.relative_rmse = 0.00000001
-    criteria.relative_fitness = 0.00000001
+            # write the cropped reconstructured mesh
+            output_path = os.path.join(scaled_mesh_path, file_obj)
+            print("Writing cropped mesh to %s"%output_path)
+            o3d.io.write_triangle_mesh(mesh=bb_pcd, 
+                    filename=output_path, write_triangle_uvs=True)
 
-    print("Apply point-to-point ICP")
-    reg_p2p = o3d.pipelines.registration.registration_icp(
-        pcd_mesh, pcd_gt_mesh, threshold, trans_init,
-        o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-        criteria=criteria
-    )
-    print(reg_p2p)
-    print("Transformation is:")
-    print(reg_p2p.transformation)
-    draw_registration_result(pcd_mesh, pcd_gt_mesh, reg_p2p.transformation)
-    mesh.transform(reg_p2p.transformation)
+    
+    # Aligning to ground truth frame
+    scaled_meshes = sorted(os.listdir(scaled_mesh_path))
+    print("\nscaled_meshes:")
+    print(scaled_meshes)
+    
+    if DEBUG:
+        scaled_meshes = [
+                #'03_sugar_box.obj',
+                #'05_mustard_bottole.obj',
+                #'06_tuna_fish_can.obj',
+                #'07_pudding_box.obj',
+                #'08_gelatin_box.obj',
+                #'10_banana.obj',
+                #'17_scissors.obj',
+                #'19_large_clamp.obj',
+                '21_foam_brick.obj'
+                ]
 
-    o3d.io.write_triangle_mesh(mesh=mesh, filename=aligned_mesh, write_triangle_uvs=True)
+    for file in scaled_meshes:
+        print("processing file : ", file)
+
+        # load scaled mesh
+        file_path = os.path.join(scaled_mesh_path, file)
+        mesh = o3d.io.read_triangle_mesh(file_path)
+
+        # prepare output filepath
+        aligned_mesh = os.path.join(aligned_mesh_path, file)
+
+        # load gt mesh
+        mesh_id = int(file.split("_")[0])
+        gt_file_path = "%s/obj_%06d.ply"%(gt_meshes, mesh_id)
+        print("gt_file_path: ", gt_file_path)
+        gt_mesh = o3d.io.read_triangle_mesh(gt_file_path)
+
+        criteria = o3d.pipelines.registration.ICPConvergenceCriteria()
+        
+        if DEBUG:
+            with open("./params.json", "r") as f:
+                data = json.load(f)
+            print(data)
+        else:
+            key = file.split(".")[0]
+            assert(key in list(icp_params.keys()))
+            data = icp_params[key]
+
+        threshold = data["threshold"]
+        criteria.max_iteration = data["criteria.max_iteration"]
+        criteria.relative_rmse = data["criteria.relative_rmse"]
+        criteria.relative_fitness = data["criteria.relative_fitness"]
+        ax = data["ax"]
+        ay = data["ay"]
+        az = data["az"]
+        tx = data["tx"]
+        ty = data["ty"]
+        tz = data["tz"]
+        num_points = data["num_points"]
+        ax, ay, az = [deg2rad(l) for l in [ax,ay,az]]
+
+        Rx = np.array([
+            [1, 0, 0],
+            [0, np.cos(ax), -np.sin(ax)],
+            [0, np.sin(ax), np.cos(ax)]])
+
+        Ry = np.array([
+            [np.cos(ay), 0, np.sin(ay)],
+            [0, 1, 0],
+            [-np.sin(ay), 0, np.cos(ay)]])
+
+        Rz = np.array([
+            [np.cos(az), -np.sin(az), 0],
+            [np.sin(az), np.cos(az), 0],
+            [0, 0, 1]])
+
+        trans_init = np.eye(4)
+        trans_init[:3,:3] = Rx @ Ry @ Rz
+        trans_init[:3,3] = np.array([tx, ty, tz])
+
+        # sub-sampled the scaled reconstructed mesh and the groundtruth one
+        pcd_mesh = mesh.sample_points_uniformly(number_of_points=num_points)
+        pcd_gt_mesh = gt_mesh.sample_points_uniformly(number_of_points=num_points)
+
+        draw_registration_result(pcd_mesh, pcd_gt_mesh, trans_init)
+
+        print("Apply point-to-point ICP")
+        reg_p2p = o3d.pipelines.registration.registration_icp(
+            pcd_mesh, pcd_gt_mesh, threshold, trans_init,
+            o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+            criteria=criteria
+        )
+        print(reg_p2p)
+        print("Transformation is:")
+        print(reg_p2p.transformation)
+        draw_registration_result(pcd_mesh, pcd_gt_mesh, reg_p2p.transformation)
+        mesh.transform(reg_p2p.transformation)
+    
+        o3d.io.write_triangle_mesh(mesh=mesh, filename=aligned_mesh, write_triangle_uvs=True)
+
+
+if __name__=="__main__":
+    main()
